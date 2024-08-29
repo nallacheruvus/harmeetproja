1321665270be3aaab998bb3fbfe590e42483f49e
nallacheruvus <41948894+nallacheruvus@users.noreply.github.com>
2024-08-29 22:08:48 +0530

    New Commit Next

--- App.java
+++ App.java
@@ -3,7 +3,10 @@ public class App{
 		System.out.println("This is a test file");
 		for(int i=1;i<=100;i++){
 			System.out.println("Log10("+i+")="+Math.log10(i));
+System.out.println("***************************");
+			System.out.println("Log("+i+")="+Math.log(i));
 		}
+		
 	}
 }
 
